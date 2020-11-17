9093d06cc6569fe4f87f37011002febf2b8cd3d7
ZolarComics <srgjzln@gmail.com>
2020-11-17 10:53:24 +1000

    Gym

---
+++ Gym.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interface
+{
+    public class Gym
+    {
+        List<Animal> animals = new List<Animal>();
+
+        public Gym()
+        {
+        }
+
+        public void AddAnimal(Animal animal)
+        {
+            animals.Add(animal);
+        }
+
+        public void LetsMove()
+        {
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if (animals[i] is IWalking)
+                {
+                    IWalking iDog = animals[i] as IWalking;
+                    iDog.Walk();
+                }
+                else if (animals[i] is IFlying)
+                {
+                    IFlying iFly = animals[i] as IFlying;
+                    iFly.Fly();
+                }
+                else if (animals[i] is ISwimming)
+                {
+                    ISwimming iFish = animals[i] as ISwimming;
+                    iFish.Swim();
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,17 +7,27 @@ namespace Interface
         static void Main(string[] args)
         {
             Dog d = new Dog("Bobik", new DateTime(2015,5,5));
-            Console.WriteLine(d.Age);
-            ((IWalking)d).Walk();
+            Console.WriteLine($"{d.Name} уже {d.Age}");
+            //((IWalking)d).Walk();
 
             Butterfly but = new Butterfly("Babka", new DateTime(2010, 10, 10));
-            ((IFlying)but).Fly();
+            Console.WriteLine($"{but.Name} уже {but.Age}");
+            //((IFlying)but).Fly();
 
             Bird bid = new Bird("Kesha", new DateTime(2007, 7, 7));
-            ((IFlying)bid).Fly();
+            Console.WriteLine($"{bid.Name} уже {bid.Age}");
+            //((IFlying)bid).Fly();
 
             Fish fis = new Fish("Nemo", new DateTime(2006, 6, 6));
-            ((ISwimming)fis).Swim();
+            Console.WriteLine($"{fis.Name} уже {fis.Age}");
+            //((ISwimming)fis).Swim();
+
+            Gym gym = new Gym();
+            gym.AddAnimal(d);
+            gym.AddAnimal(but);
+            gym.AddAnimal(bid);
+            gym.AddAnimal(fis);
+            gym.LetsMove();
 
             Console.ReadKey();
         }
